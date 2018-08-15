62b072003a97ce5c6a07b459045d6eb159e7ae89
Zzolushka <42086878+Zzolushka@users.noreply.github.com>
2018-08-15 15:22:29 +0300

    Добавьте файлы проекта.

---
+++ EnginerWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EnginerWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
